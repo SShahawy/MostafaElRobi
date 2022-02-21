58a48b396c56646d8b2ddcee15f4e0a5848c6111
SherifDev <storm_wo@hotmail.com>
2022-02-21 13:30:01 +0200

    left

---
+++ resources/views/pages/email/dynamic_email2.blade.php
@@ -0,0 +1,7 @@
+
+<p style="text-align: right;">الاسم :  {{ $data['name'] }}</p>
+<p style="text-align: right;">الإيميل :{{ $data['email'] }}.</p>
+<p style="text-align: right;">رقم التليفون :{{ $data['phone'] }}.</p>
+<p style="text-align: right;">النوع :{{ $data['gender'] }}.</p>
+<p style="text-align: right;">تفاصيل : {{ $data['details'] }}.</p>
+<p style="text-align: left;">توظيف و تدريب - مصطفي الروبي | محامون . مستشارون</p>
