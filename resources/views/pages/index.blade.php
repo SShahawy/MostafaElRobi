2c78752ccd5d748b179f1cd0168895412a4ad8b6
SherifDev <storm_wo@hotmail.com>
2022-02-21 16:09:42 +0200

    new

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1009,16 +1009,21 @@
                     </div>
                     <div class="carousel-item">
                         <div class="testimonial4_slide">
-                            <img src="https://i.ibb.co/8x9xK4H/team.jpg" class="img-circle img-responsive" /><p>CLALSDLASD 2 </p>
-                            <h4>Client 2</h4>
+                            <img src="images/testimon/testim1.jpg" class="img-circle img-responsive" />
+                            <p>شرف لشركتي ان المؤسسه هي المستشار القانوني لنا علاقتها بكافه القطاعات الحكوميه و هيئه الأستثمار ذللت لنا الكثير من العقبات قبل و اثناء العمل مما عاد علي شركتي بوفير الجهد و اقل نفقات ممكنه و الأستمرار في الانتاج دون ضغوط او إشكالات قانونيه</p>
+                            <h4>أ/ محمد كمال</h4>
+                            <h4>المدير التنفيزي لشركه جولد كيرافارما</h4>
+                            <h4>2</h4>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <div class="testimonial4_slide">
-                            <img src="https://i.ibb.co/8x9xK4H/team.jpg" class="img-circle img-responsive" />
-                            <p>CLALSDLASD 3</p>
-                            <h4>Client 3</h4>
-                            <h3>Client 2</h3>
+                            <img src="images/testimon/testim1.jpg" class="img-circle img-responsive" />
+
+                            <p>شرف لشركتي ان المؤسسه هي المستشار القانوني لنا علاقتها بكافه القطاعات الحكوميه و هيئه الأستثمار ذللت لنا الكثير من العقبات قبل و اثناء العمل مما عاد علي شركتي بوفير الجهد و اقل نفقات ممكنه و الأستمرار في الانتاج دون ضغوط او إشكالات قانونيه</p>
+                            <h4>أ/ محمد كمال</h4>
+                            <h4>المدير التنفيزي لشركه جولد كيرافارما</h4>
+                            <h4>3</h4>
                         </div>
                     </div>
                 </div>
